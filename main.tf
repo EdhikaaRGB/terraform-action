e6a1a111aa3c052ae9d73623b314af78b1fc1144
EdhikaaRGB <120156563+EdhikaaRGB@users.noreply.github.com>
2023-01-05 14:06:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-resource "aws_ecs_cluster" "cluster" {
-  name = "${var.project}-${var.environment}"
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
+#resource "aws_ecs_cluster" "cluster" {
+#  name = "${var.project}-${var.environment}"
+#  setting {
+#    name  = "containerInsights"
+#    value = "enabled"
+#  }
+#  capacity_providers = ["FARGATE", ]
+# }
+
+
+resource "aws_instance" "myec2" {
+  ami = "ami-0cca134ec43cf708f"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.project}-${var.environment}"
   }
-  capacity_providers = ["FARGATE", ]
 }
